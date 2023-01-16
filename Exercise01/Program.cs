eef3b1f23a4d0891a9fa438925be3300568f1f7a
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-16 18:54:21 +0300

    Add Exercise01 max_min

---
+++ Exercise01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: 
+
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+System.Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    System.Console.Write($"число {a} > max  ");
+    System.Console.Write($"число {b} > min  ");
+}
+else
+{
+    System.Console.Write($"число {b} > max  ");
+    System.Console.Write($"число {a} > min  ");
+}
