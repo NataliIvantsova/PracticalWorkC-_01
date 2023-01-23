d6791c74a0f05a770e8f99b1126feea34e4505f4
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-23 19:53:03 +0300

    Внесены новые решения в задачу 4

--- Exercise04/Program.cs
+++ Exercise04/Program.cs
@@ -3,12 +3,44 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+//!!! Мое решение!!
+// System.Console.WriteLine("Введите число > ");
 
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 2; i <= n; i+=2)
+// if (i % 2 == 0)
+// Console.Write($"{i} ");
+
+//НЕ Мое решение!!
+// System.Console.WriteLine("Введите число > ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int count =2;// вместо i введен другой индекс
+// string Numbers = "";
+// string separator = "";
+// while (count <= n)
+// {
+//     if(count>2)
+//     {
+//         Numbers = Numbers + separator + count;
+//     }
+//     count+=2;
+// }
+
+// System.Console.WriteLine($"четные числа: {Numbers} ");
+
+
+//!!! Мое решение №2!!
 System.Console.WriteLine("Введите число > ");
 
 int n = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 2; i <= n; i+=2)
-if (i % 2 == 0)
-Console.Write($"{i} ");
+int i = 2;
+while(i <= n)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+    i+=2;
+}
 
